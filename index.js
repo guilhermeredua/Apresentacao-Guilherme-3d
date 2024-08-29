e05f822d8f3547e711c7cd7635b8de4d19c1f520
guilhermeredua <127844688+guilhermeredua@users.noreply.github.com>
2024-08-29 01:28:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const primeiroNome = "Guilherme Henrique";
+const sobrenome = "Diniz Gemeno Rédua"
+const idade = "17 anos";
+const altura = "1,70m de altura";
+const massa = "66kg";
+const cidadeNatal = "Curitiba";
+
+console.log("Os meus primeiros nomes são:");
+console.log(primeiroNome);
+
+console.log("Os sobrenomes das minhas famílias são:");
+console.log(sobrenome);
+
+console.log("Minha idade é:");
+console.log(idade);
+
+console.log("Tenho:");
+console.log(altura);
+
+console.log("Peso:");
+console.log(massa);
+
+console.log("Nasci em:")
+console.log(cidadeNatal);
